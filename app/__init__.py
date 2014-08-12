168c411684016179f5bd815e75f99ab8bd065eaa
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2014-08-12 08:17:41 -0500

    mongo

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from pymongo import MongoClient
 import os
 # from flask.ext.pymongo import PyMongo
-
 from flask_sslify import SSLify
+
+# Format: MONGOHQ_URL: mongodb://<user>:<pass>@<base_url>:<port>/<url_path>
+if os.environ.get('MONGOHQ_URL'):
+    client = MongoClient(os.environ['MONGOHQ_URL'])
+else:
+    client = MongoClient()
+
 
 app = Flask(__name__)
 app.config.from_object('config')
-client = MongoClient()
 db = client.wikilango
 # mongo = PyMongo(app)
 # sslify = SSLify(app)
