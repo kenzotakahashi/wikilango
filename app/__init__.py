0e6fc0aa40c07841aa0b50c2f7bf2f39ffdb15aa
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2014-08-12 09:54:51 -0500

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from pymongo import MongoClient
 import os
 # from flask.ext.pymongo import PyMongo
 from flask_sslify import SSLify
+import logging
+from logging import StreamHandler
+
+app = Flask(__name__)
 
 # Format: MONGOHQ_URL: mongodb://<user>:<pass>@<base_url>:<port>/<url_path>
 if os.environ.get('MONGOHQ_URL'):
@@ -11,8 +15,11 @@ else:
     client = MongoClient()
 
 
-app = Flask(__name__)
-app.debug = True
+file_handler = StreamHandler()
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
+
+
 app.config.from_object('config')
 db = client.wikilango
 # mongo = PyMongo(app)
--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 # SECRET_KEY = 'you-will-never-guess'
 
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
